baf5b9a665e10a3afabefe8ffc0d109a7edf8aa7
Yukiua <66011734+Yukiua@users.noreply.github.com>
2022-01-13 15:38:07 +0800

    doctor report

---
+++ HealthUP/wwwroot/css/report.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HealthUP.wwwroot.css
+{
+    public class reportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
